1ca97b03d3f15fb479c961de3f8771b0c141d22d
kkAyataka <kk.ayataka@gmail.com>
2025-09-14 21:16:22 +0900

    chore: adjust __init__

--- src/markdiff/_git_diff/__init__.py
+++
@@ -1,8 +0,0 @@
-from .git_diff import WordDiff, run_git_diff
-from .parse_porcelain_diff import parse_porcelain_diff
-
-__all__ = [
-    "WordDiff",
-    "run_git_diff",
-    "parse_porcelain_diff",
-]
